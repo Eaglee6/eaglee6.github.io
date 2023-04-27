2e2100fc7da7c662b31868d281b38cc50f4d4aa4
Eaglee6 <117346766+Eaglee6@users.noreply.github.com>
2023-04-27 11:12:14 +0300

    Create p16.js

---
+++ ask16/p16.js
@@ -0,0 +1,61 @@
+function athlitika() {
+document.getElementById("περιεχόμενο").innerHTML = '\
+<p><a href="http://www.segas.gr/" target="_blank">Σύνδεσμος Ελληνικών Γυμναστικών
+Αθλητικών Σωματείων</a></p>\
+<p><a href="http://www.olympic.org/" target="_blank">Διεθνής Ολυμπιακή
+Επιτροπή</a></p>\
+<p><a href="http://www.fiba.com/" target="_blank">Διεθνής Ομοσπονδία
+Καλαθοσφαίρισης</a></p>
+';
+}
+function diastima() {
+document.getElementById("περιεχόμενο").innerHTML = '\
+<p><a href="http://www.nasa.gov/" target="_blank">National Aeronautics and Space
+Administration</a></p>\
+<p><a href="www.esa.int" target="_blank">European Space Agency</a></p>
+';
+}
+function ekpaideysi() {
+document.getElementById("περιεχόμενο").innerHTML = '\
+<p><a href="http://www.eie.gr/" target="_blank">Εθνικό Ίδρυμα Ερευνών</a></p>\
+<p><a href="http://www.eugenfound.edu.gr/" target="_blank">Ίδρυμα Ευγενίδου</a></p>\
+<p><a href="https://www.khanacademy.org/" target="_blank">Khan Academy</a></p>\
+<p><a href="http://www.wolframalpha.com/" target="_blank">Wolfram Alpha</a></p>\
+';
+}
+function periballon() {
+document.getElementById("περιεχόμενο").innerHTML = '\
+<p><a href="http://www.arcturos.gr/" target="_blank">Αρκτούρος</a></p>\
+<p><a href="http://www.greenpeace.org/greece/el/" target="_blank">Greenpeace</a></p>\
+<p><a href="http://www.wwf.gr/" target="_blank">WWF</a></p>\
+';
+}
+function politismos() {
+document.getElementById("περιεχόμενο").innerHTML = '\
+<p><a href="http://www.theacropolismuseum.gr/" target="_blank">Μουσείο
+Ακρόπολης</a></p>\
+<p><a href="http://mbp.gr/" target="_blank">Μουσείο Βυζαντινού Πολιτισμού</a></p>\
+<p><a href="http://www.aigai.gr/" target="_blank">Μουσείο Βασιλικών Τάφων των Αιγών -
+Βεργίνα</a>\
+<p><a href="http://www.ime.gr/" target="_blank">Ίδρυμα Μείζονος Ελληνισμού</a></p>\
+<p><a href="http://en.unesco.org/" target="_blank">UNESCO</a></p>\
+';
+}
+function texnologia() {
+document.getElementById("περιεχόμενο").innerHTML = '\
+<p><a href="http://www.certh.gr/" target="_blank">Εθνικό Κέντρο Έρευνας και
+Τεχνολογικής Ανάπτυξης</a></p>\
+<p><a href="http://www.cti.gr/" target="_blank">Ινστιτούτο Τεχνολογίας Υπολογιστών και
+Εκδόσεων</a></p>\
+<p><a href="http://www.noesis.edu.gr/" target="_blank">Κέντρο Διάδοσης Επιστημών και
+Μουσείο Τεχνολογίας</a></p>\
+<p><a href="https://www.forth.gr/" target="_blank">Ίδρυμα Τεχνολογίας και
+Έρευνας</a></p>\
+<p><a href="http://www.iti.gr/" target="_blank">Ινστιτούτο Τεχνολογιών Πληροφορικής και
+Επικοινωνιών</a></p>\
+<p><a href="http://www.itap-nagref.gr/" target="_blank">Ινστιτούτο Τεχνολογίας
+Γεωργικών Προϊόντων</a></p>\
+<p><a href="http://web.mit.edu/" target="_blank">Massachusetts Institute of
+Technology</a></p>\
+';
+}
